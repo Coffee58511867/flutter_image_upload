261551becbe951f1d9ccfc7f7339b93130ede5c4
Coffee58511867 <marobahlalele910@gmail.com>
2023-06-16 07:29:05 +0200

    Create file upload page

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:image_upload/pages/upload.dart';
+import 'package:image_upload/pages/upload_file.dart';
 import 'package:image_upload/pages/view_uploads.dart';
 
 class TabPage extends StatefulWidget {
@@ -12,18 +13,19 @@ class TabPage extends StatefulWidget {
 class _TabPageState extends State<TabPage> {
   @override
   Widget build(BuildContext context) => DefaultTabController(
-        length: 2,
+        length: 3,
         child: Scaffold(
           appBar: AppBar(
             title: const Text("Dashboard"),
             centerTitle: true,
             bottom: const TabBar(tabs: [
               Tab(text: "Upload"),
+              Tab(text: "Files"),
               Tab(text: "My Uploads"),
             ]),
           ),
           body: const TabBarView(
-            children: [UploadImagePage2(), UploadsPage()],
+            children: [UploadImagePage2(), UploadFilesPage(), UploadsPage()],
           ),
         ),
       );
